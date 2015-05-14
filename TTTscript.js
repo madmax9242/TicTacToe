337292c9815591e7a4522c46d1023fdc4a939db6
Max DePriest <maxd924@gmail.com>
2015-05-14 11:50:58 -0500

    Added a ternary operator that picks who goes first

--- TTTscript.js
+++ TTTscript.js
@@ -1,9 +1,12 @@
-var xIcon = '<span><i class="fa fa-times fa-5x"></i></span>';
-var oIcon = '<p><i class="fa fa-circle-o fa-5x"></i></p>';
-var turnCounter = 0;
+var xIcon = '<i class="fa fa-times fa-5x"></i>';
+var oIcon = '<i class="fa fa-circle-o fa-5x"></i>';
+
 $(document).ready(function() {
-	
+	var turnCounter = Math.random() < 0.5 ? 1 : 0;
+	//Chooses the order of play.  If it's a 1, X goes first.  If its a 0, O goes first
 	$('td').click(function(event)  {
+		console.log(turnCounter);
+		turnCounter++;
 		if (turnCounter % 2 === 0) {
 			if ($(this).html() == xIcon || $(this).html() == oIcon) {
 
@@ -13,8 +16,7 @@ $(document).ready(function() {
 			else {
 				//I add the class to the table cell so I can use it to compare in the checkWinner function
 				$(this).html(xIcon);
-				$(this).addClass('xClass'); 
-				turnCounter++;
+				$(this).addClass('xClass'); 				
 				checkWinner();
 			}
 		}
@@ -28,91 +30,70 @@ $(document).ready(function() {
 			else {
 				$(this).html(oIcon);
 				$(this).addClass('oClass');
-				turnCounter++;
 				checkWinner();
 			}
 		}
 	})
 
+	function chooseOrderofPlay()  {
+		var random = Math.random();
+		var turnCounter = random < 0.5 ? 1 : 0;
+		console.log(random)
+		console.log(turnCounter);
+		return turnCounter;
+		// var random = Math.random();
+		// console.log(random);
+		// if (random < 0.5) {
+		// 	turnCounter = 1;
+		// 	return turnCounter;
+		// }
+		// return turnCounter;
+		//}
+
+	}
+
 	function checkWinner()  {
 		var tester = ['oClass','xClass'];
-		var victoryConfirm = 0
+		var victoryConfirm = 0;
 
 		for (var i = 0; i < tester.length; i++) {
 			if ($('#alpha').hasClass(tester[i]) == true && $('#bravo').hasClass(tester[i]) == true && ($('#charlie').hasClass(tester[i]) == true))  {
-				if (tester[i] == 'oClass')  {
-					alert("Player 2 wins");
-					location.reload();
-				}
-				else {
-					alert("Player 1 wins");
-					location.reload();
-				}
-				// victoryConfirm = 1
+				victoryConfirm = 1
 			}
 			else if ($('#delta').hasClass(tester[i]) == true && $('#epsilon').hasClass(tester[i]) == true && ($('#foxtrot').hasClass(tester[i]) == true)) {
-				if (tester[i] == 'oClass')  {
-					alert("Player 2 wins");
-				}
-				else {
-					alert("Player 1 wins");
-				}
+				victoryConfirm = 1
 			}
 			else if ($('#golf').hasClass(tester[i]) == true && $('#hotel').hasClass(tester[i]) == true && ($('#indigo').hasClass(tester[i]) == true)) {
-				if (tester[i] == 'oClass')  {
-					alert("Player 2 wins");
-				}
-				else {
-					alert("Player 1 wins");
-				}				
+				victoryConfirm = 1				
 			}
 			else if ($('#alpha').hasClass(tester[i]) == true && $('#delta').hasClass(tester[i]) == true && ($('#golf').hasClass(tester[i]) == true)) {
-				if (tester[i] == 'oClass')  {
-					alert("Player 2 wins");
-				}
-				else {
-					alert("Player 1 wins");
-				}
+				victoryConfirm = 1
 			}
 			else if ($('#bravo').hasClass(tester[i]) == true && $('#epsilon').hasClass(tester[i]) == true && ($('#hotel').hasClass(tester[i]) == true)) {
-				if (tester[i] == 'oClass')  {
-					alert("Player 2 wins");
-				}
-				else {
-					alert("Player 1 wins");
-				}
+				victoryConfirm = 1
 			}
 			else if ($('#charlie').hasClass(tester[i]) == true && $('#foxtrot').hasClass(tester[i]) == true && ($('#indigo').hasClass(tester[i]) == true)) {
-				if (tester[i] == 'oClass')  {
-					alert("Player 2 wins");
-				}
-				else {
-					alert("Player 1 wins");
-				}
+				victoryConfirm = 1
 			}
 			else if ($('#alpha').hasClass(tester[i]) == true && $('#epsilon').hasClass(tester[i]) == true && ($('#indigo').hasClass(tester[i]) == true)) {
-				if (tester[i] == 'oClass')  {
-					alert("Player 2 wins");
-				}
-				else {
-					alert("Player 1 wins");
-				}
+				victoryConfirm = 1
 			}
 			else if ($('#charlie').hasClass(tester[i]) == true && $('#epsilon').hasClass(tester[i]) == true && ($('#golf').hasClass(tester[i]) == true)) {
-				if (tester[i] == 'oClass')  {
-					alert("Player 2 wins");
-				}
-				else {
-					alert("Player 1 wins");
-				}
+				victoryConfirm = 1
 			}
 
-			// if (tester[i] == 'oClass' && victoryConfirm == 1)  {
-			// 	alert("Player 2 wins");
-			// }
-			// else if (tester[i] == 'xClass' && victoryConfirm == 1) {
-			// 	alert("Player 1 wins");
-			//}
+			if (tester[i] == 'oClass' && victoryConfirm == 1)  {
+				victoryConfirm = 0;
+				alert("Player 2 wins");
+				location.reload();
+
+			}
+			else if (tester[i] == 'xClass' && victoryConfirm == 1) {
+				victoryConfirm = 0;
+				alert("Player 1 wins");
+				location.reload();
+
+			}
 		}
 	}
 });
